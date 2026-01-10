8a9862be16de03fc9d2d40c7f9653c29e6020ffa
Reketino <bjornevensk8@gmail.com>
2026-01-10 11:22:18 +0100

    Made auth.py & reverted main script back, so its still operating during refactor

---
+++ Opensky/auth.py
@@ -0,0 +1,24 @@
+import requests
+from .config import OPENSKY_CLIENT_ID, OPENSKY_CLIENT_SECRET
+
+OAUTH_TOKEN_URL = (
+    "https://auth.opensky-network.org/auth/realms/"
+    "opensky-network/protocol/openid-connect/token"
+)
+
+def get_opensky_token() -> str:
+    res = requests.post(
+        OAUTH_TOKEN_URL,
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data={
+            "grant_type": "client_credentials",
+            "client_id": OPENSKY_CLIENT_ID,
+            "client_secret": OPENSKY_CLIENT_SECRET,
+        },
+        timeout=20,
+    )
+    
+    res.raise_for_status()
+    return res.json()["Bears access_token"]
