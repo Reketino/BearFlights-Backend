757d8d0b6b8560f206f38c57dcd03217a6fc7801
Reketino <bjornevensk8@gmail.com>
2026-01-13 23:03:11 +0100

    Added comments to functions in airline.py & one comment on the role of airlines_icao script

--- opensky/airline.py
+++ opensky/airline.py
@@ -1,12 +1,14 @@
 from opensky.airlines_icao import ICAO_AIRLINES
 
+# Airline function w/ input intake 
 def airline_from_callsign(callsign: str) -> tuple [str, str] | None:
     if not callsign or len(callsign) < 3:
         return None
     
-    icao = callsign[:3].upper()
-    airline =ICAO_AIRLINES.get(icao)
-
+    icao = callsign[:3].upper() # Collecting ICAO code
+    airline = ICAO_AIRLINES.get(icao) # Check ICAO_AIRLINES for names
+    
+    # Returning nothing if no Airline is found
     if not airline:
         return None
 
