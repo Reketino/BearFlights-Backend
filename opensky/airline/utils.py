083ef8a3270d50577050ac526f2c40de3c8b906b
Reketino <bjornevensk8@gmail.com>
2026-01-25 16:03:00 +0100

    Refactor: utils now sorts airline list alphabetical

--- opensky/airline/utils.py
+++ opensky/airline/utils.py
@@ -1,8 +1,11 @@
-from opensky.airline.airlines_icao import ICAO_AIRLINES
+from opensky.airline.airlines_icao import AIRLINES_BY_REGION
 
 def sorted_airlines() -> None:
-    for key, value in sorted(ICAO_AIRLINES.items()):
-        print(f'"{key}": "{value}",')
+    for region, airlines in AIRLINES_BY_REGION.items():
+        print(f"# {region}")
+        for code, name in sorted (airlines.items(), key=lambda x: x[1]):
+            print(f'"{code}": "{name}",')
+        print()
         
 if __name__ == "__main__":
     sorted_airlines()
