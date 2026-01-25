d1b0ed7bbd24db628d1bdf81db97a70537dee98f
Reketino <bjornevensk8@gmail.com>
2026-01-25 15:46:02 +0100

    Feat: sorting script for airlines

--- opensky/airline/airlines_icao.py
+++ opensky/airline/airlines_icao.py
@@ -1,22 +1,37 @@
 
 # Callsign of airline companies defined
-ICAO_AIRLINES: dict[str, str] = {
+AIRLINES_BY_REGION: dict[str, dict[str, str]] = {
+    
+    "Nordic": {
     "SAS": "Scandinavian Airlines",
     "NOZ": "Norwegian Air Shuttle",
     "WIF": "Widerøe",
-    "QTR": "Qatar Airways",
-    "UAE": "Emirates",
+    "FIN": "Finnair",
+    "ICE": "Icelandair",
+    },
+    "Europe": {
     "THY": "Turkish Airlines",
     "EZY": "easyJet",
-    "FIN": "Finnair",
     "BAW": "British Airways",
     "DLH": "Lufthansa",
     "KLM": "KLM Royal Dutch Airlines",
     "AFR": "Air France",
-    "ICE": "Icelandair",
     "BTI": "airBaltic",
-    "ETD": "Etihad Airways",
     "EIN": "Aer Lingus",
-    "RRR": "UK Royal Air Force",
     "RYR": "Ryanair",
+    },
+    "Middle East": {
+    "QTR": "Qatar Airways",
+    "UAE": "Emirates", 
+    "ETD": "Etihad Airways",   
+    },
+    "Military / State": {
+    "RRR": "UK Royal Air Force",    
+    } 
+}
+
+ICAO_AIRLINES: dict[str, str] = {
+    code: name
+    for group in AIRLINES_BY_REGION.values()
+    for code, name in group.items()
 }
