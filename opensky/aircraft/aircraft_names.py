b671b08b71b68c16844ef26f4f4bd1fcbdb67c37
Reketino <bjornevensk8@gmail.com>
2026-01-25 11:02:35 +0100

    Feat: new aircraft types

--- opensky/aircraft/aircraft_names.py
+++ opensky/aircraft/aircraft_names.py
@@ -7,18 +7,23 @@ AIRCRAFT_TYPES: dict[str, str] = {
     "A340": "Airbus A340-200",
     "A400": "Airbus A400",
     "A21N": "Airbus A321neo",
+    "A359": "Airbus A350-900",
+    "A35K": "Airbus A350-1000",
+    "BCS3": "Airbus A220-300",
      
-    
     #Boeing
     "B789": "Boeing 787",
     "B39M": "Boeing 737 MAX 9",
     "B77L": "Boeing 777",
-    
+    "B738": "Boeing 737-800",
     
     #Embraer
     "E290": "Embraer E190-E2",
-    
-    
+    "E55P": "Embraer Phenom 300",
+
     #De Havilland Canada
-    "DH8D": " De Havilland Canada",
+    "DH8D": "De Havilland Canada DHC-8-400",
+    
+    #Cessna 
+    "C25A": "Cessna 525A CJ2",
 }
