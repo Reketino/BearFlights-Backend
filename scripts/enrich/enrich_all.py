ce49df0c07481e059d41226a08bf509351143205
Reketino <bjornevensk8@gmail.com>
2026-01-28 16:07:11 +0100

    Feat: enrich_airports added to enrich_all, will now run w/ ghub actions

--- scripts/enrich/enrich_all.py
+++ scripts/enrich/enrich_all.py
@@ -1,5 +1,6 @@
 from scripts.enrich.enrich_aircraft_types import enrich_aircraft_types
 from scripts.enrich.enrich_airlines import enrich_airlines
+from scripts.enrich.enrich_airports import enrich_airports
 
 def main():
     try:
@@ -11,6 +12,11 @@ def main():
         enrich_aircraft_types(limit=100)
     except Exception as e:
         print("[WARN] enrichment of aircrafts failed, skip🦘:", e)
+    
+    try:
+        enrich_airports(limit=100)
+    except Exception as e:
+        print("[WARN] enrichment of airports failed, skip:", e)
     
 
 if __name__ == "__main__":
