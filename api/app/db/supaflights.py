e0145698cff3eeb66205a08487e85df332161eb4
Reketino <bjornevensk8@gmail.com>
2026-02-01 11:22:33 +0100

    Feat: setup & defined list for collecting flights to api

---
+++ api/app/db/supaflights.py
@@ -0,0 +1,22 @@
+from typing import Any, cast
+from api.app.db.supabase import supabase
+
+def fetch_flights(limit: int = 50) -> list[dict[str, Any]]:
+    res = (
+        supabase
+        .table("flights")
+        .select(
+            "icao24,"
+            "callsign,"
+            "aircraft_name,"
+            "departure_airport,"
+            "arrival_airport,"
+            "arrival_airport_name,"
+            "distance_over_area"
+        )
+        .order("first_seen", desc=True)
+        .limit(limit)
+        .execute()
+    )
+    
+    return cast(list[dict[str, Any]], res.data or [])
