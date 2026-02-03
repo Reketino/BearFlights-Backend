80592acdb38a365c0f90e802918d5971b4b8e657
Reketino <bjornevensk8@gmail.com>
2026-02-03 16:04:35 +0100

    Feat: connected rate-limiter to script + fixed typing & marked parameter as "used"

--- api/app/routes/aircraft.py
+++ api/app/routes/aircraft.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Request
-from opensky.aircraft_AI.generate import aircraft_generated_description
-
+from opensky.aircraft_ai.generate import aircraft_generated_description
+from app.ai_rate_limiter import ai_rate_limiter
 
 router = APIRouter(
     prefix="/aircraft",
@@ -13,11 +13,13 @@ router = APIRouter(
     description="This AI model generates a live description for a give aircraft IACO type. w/ rate-limit.",
 )
 
-@rate_limit(limit=3, per=60)
+@ai_rate_limiter(limit=3, per=60)
 def ai_aircraft_description(
     icao: str,
     request: Request,
 ):
+    _ = request
+     
     try:
         description = aircraft_generated_description(icao.upper())
         return {
