4508bd7305a5d07fb310ad499ea7f0b9b1c766c3
Reketino <bjornevensk8@gmail.com>
2026-02-08 16:52:36 +0100

    Feat: cors.middleware imported working on setup

--- api/app/main.py
+++ api/app/main.py
@@ -2,9 +2,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.app.routes.aircraft import router as aircraft_router
 
 app = FastAPI(title="BearFlights AI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:300"
+    ]
+)
 
 app.include_router(
     aircraft_router,
