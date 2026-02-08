86becac5acf2780d2044058e5546c061ce67874d
Reketino <bjornevensk8@gmail.com>
2026-02-08 11:19:43 +0100

    Refactor: set up gemini for AI description of flight type

--- opensky/aircraft_AI/generate.py
+++ opensky/aircraft_AI/generate.py
@@ -1,12 +1,15 @@
+
+from google.genai import types
 from .client import client
 from .prompt import aircraft_prompt
 
 def aircraft_generated_description(icao: str) -> str:
-    response = client.responses.generate_content(
-        model="gemini-1.5 flash",
-        input= aircraft_prompt(icao),
+    response: types.GenerateContentResponse = client.models.generate_content( # type: ignore
+        model="gemini-1.5-flash",
+        contents=aircraft_prompt(icao)   
     )
     if not response.text: 
         raise RuntimeError("Zero response from from Gemini")
+    
     
     return response.text.strip()
