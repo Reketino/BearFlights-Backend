5fe64f1e754eaf4c5a85f4928277e3f042007049
Reketino <bjornevensk8@gmail.com>
2026-02-13 12:54:57 +0100

    Feat: script for deleting old flight data from Supabase, to keep the dataset manageable and relevant.

--- scripts/cleanup_old_positions.py
+++ scripts/cleanup_old_positions.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 supbase = create_client(
     os.environ["SUPABASE_URL"],
-    os.environ["SUPBASE_SERVICE_ROLE_KEY"],
+    os.environ["SUPABASE_SERVICE_ROLE_KEY"],
 )
 
 def cleanup_old_positions() -> None:
@@ -21,4 +21,9 @@ def cleanup_old_positions() -> None:
     )
     
     print("Cleanup process completed.")
+    print("Rows Removed:", len(res.data)or [])
+    
+    
+if __name__ == "__main__":
+    cleanup_old_positions()
     
