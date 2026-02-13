8c4d7f5ccf30cd4cc5171040e726126c748a4563
Reketino <bjornevensk8@gmail.com>
2026-02-13 11:57:42 +0100

    Fix: script for sorting fixed & airports sorted⛱️

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -1,136 +1,131 @@
+# Airports sorted by country & name
 AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
-    # Scandinavia
-    "ENAL": {
-      "name": "Aalesund Airport",
-      "country": "Norway",
-      "iata": "AES",  
-    },
-      
-    "ENVA": {
-      "name": "Trondheim, Vaernes Airport",
-      "country": "Norway",
-      "iata": "TRD",  
-    },
-    
-    "ENGM": {
-        "name": "Oslo Gardermoen Airport",
-        "country": "Norway",
-        "iata": "OSL",  
-    },
-    
-    "ENBR": {
-      "name": "Bergen Flesland Airport",
-      "country": "Norway",
-      "iata": "BGO",  
-    },
-    
-    "ENHD": {
-        "name": "Haugesund Airport",
-        "country": "Norway",
-        "iata": "HAU",
-    },
-     
-    "ENSR": {
-        "name": "Sørkjosen Airport ",
-        "country": "Norway",
-        "iata": "SOJ",
-    },
-     
-    "ENSD": {
-        "name": "Sandane, Anda Airport",
-        "country": "Norway",
-        "iata": "SDN",
-    },
-     
-    "ENBL": {
-        "name": "Førde Airport",
-        "country": "Norway",
-        "iata": "FDE",
-    }, 
-     
-    "ENOV": {
-        "name": "Orsta Volda Airport",
-        "country": "Norway",
-        "iata": "HOV",
-    },
-         
-    "EKCH": {
-        "name": "Copenhagen Airport",
-        "country": "Denmark",
-        "iata": "CPH",
-    },
-     
-    "BIKF": {
-        "name": "Keflavík International Airport",
-        "country": "Iceland",
-        "iata": "KEF",
-    },
-      
-    "EFHK": {
-        "name": "Helsinik Airport",
-        "country": "Finland",
-        "iata": "HEL",
-    },
-    
-    "EFPJ": {
-        "name": "Jäkäläpää Airfield",
-        "country": "Finland",
-        "iata": "",
-    },
-    
-    # Europe
-    "EGCC": {
-        "name": "Manchester Airport",
-        "country": "United Kingdom",
-        "iata": "MAN",
-    },
-    
-    "EGGW": {
-        "name": "London Luton Airport",
-        "country": "United Kingdom",
-        "iata": "LTN",
-    },
-     
-    "EGKK": {
-        "name": "London Gatwick Airport",
-        "country": "United Kingdom",
-        "iata": "LGW",
-    },
-    
-    "EGVN": {
-        "name": "RAF Brize Norton",
-        "country": "United Kingdom",
-        "iata": "BZZ",
-    },
-     
-    "EVRA": {
-        "name": "Riga International Airport",
-        "country": "Latvia",
-        "iata": "RIX",  
-    },
-    
-    "GCFV": {
-        "name": "Fuerteventura Airport",
-        "country": "Spain",
-        "iata": "FUE",
-    },
-    
+
+    # Bulgaria
     "LBLS": {
         "name": "Lesnovo Airfield",
         "country": "Bulgaria",
         "iata": "",
-    },
-    
-    #United States of America
+   },
+
+    # Denmark
+    "EKCH": {
+        "name": "Copenhagen Airport",
+        "country": "Denmark",
+        "iata": "CPH",
+   },
+
+    # Finland
+    "EFHK": {
+        "name": "Helsinik Airport",
+        "country": "Finland",
+        "iata": "HEL",
+   },
+    "EFPJ": {
+        "name": "Jäkäläpää Airfield",
+        "country": "Finland",
+        "iata": "",
+   },
+
+    # Iceland
+    "BIKF": {
+        "name": "Keflavík International Airport",
+        "country": "Iceland",
+        "iata": "KEF",
+   },
+
+    # Latvia
+    "EVRA": {
+        "name": "Riga International Airport",
+        "country": "Latvia",
+        "iata": "RIX",
+   },
+
+    # Norway
+    "ENAL": {
+        "name": "Aalesund Airport",
+        "country": "Norway",
+        "iata": "AES",
+   },
+    "ENBR": {
+        "name": "Bergen Flesland Airport",
+        "country": "Norway",
+        "iata": "BGO",
+   },
+    "ENBL": {
+        "name": "Førde Airport",
+        "country": "Norway",
+        "iata": "FDE",
+   },
+    "ENHD": {
+        "name": "Haugesund Airport",
+        "country": "Norway",
+        "iata": "HAU",
+   },
+    "ENOV": {
+        "name": "Orsta Volda Airport",
+        "country": "Norway",
+        "iata": "HOV",
+   },
+    "ENGM": {
+        "name": "Oslo Gardermoen Airport",
+        "country": "Norway",
+        "iata": "OSL",
+   },
+    "ENSD": {
+        "name": "Sandane, Anda Airport",
+        "country": "Norway",
+        "iata": "SDN",
+   },
+    "ENSR": {
+        "name": "Sørkjosen Airport ",
+        "country": "Norway",
+        "iata": "SOJ",
+   },
+    "ENVA": {
+        "name": "Trondheim, Vaernes Airport",
+        "country": "Norway",
+        "iata": "TRD",
+   },
+
+    # Spain
+    "GCFV": {
+        "name": "Fuerteventura Airport",
+        "country": "Spain",
+        "iata": "FUE",
+   },
+
+    # United Kingdom
+    "EGKK": {
+        "name": "London Gatwick Airport",
+        "country": "United Kingdom",
+        "iata": "LGW",
+   },
+    "EGGW": {
+        "name": "London Luton Airport",
+        "country": "United Kingdom",
+        "iata": "LTN",
+   },
+    "EGCC": {
+        "name": "Manchester Airport",
+        "country": "United Kingdom",
+        "iata": "MAN",
+   },
+    "EGVN": {
+        "name": "RAF Brize Norton",
+        "country": "United Kingdom",
+        "iata": "BZZ",
+   },
+
+    # United States of America
     "KJFK": {
         "name": "John F. Kennedy International Airport",
         "country": "United States of America",
         "iata": "JFK",
-    },
-    
+   },
     "KMIA": {
         "name": "Miami International Airport",
         "country": "United States of America",
         "iata": "MIA",
-    },
+   },
 }
