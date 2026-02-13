e8ea20f76b2aa219925ffc2a6f6eb96392d16f61
Reketino <bjornevensk8@gmail.com>
2026-02-13 12:50:34 +0100

    Feat: Logic behind deleting olf flight data from Supabase set up

--- scripts/cleanup_old_positions.py
+++ scripts/cleanup_old_positions.py
@@ -3,3 +3,22 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+supbase = create_client(
+    os.environ["SUPABASE_URL"],
+    os.environ["SUPBASE_SERVICE_ROLE_KEY"],
+)
+
+def cleanup_old_positions() -> None:
+    print("Cleanup old positions process initiated...")
+    
+    res = (
+        supbase
+        .table("flight_positions")
+        .delete()
+        .lt("last_seen", "now() - interval '7 days'")
+        .execute()
+    )
+    
+    print("Cleanup process completed.")
+    
