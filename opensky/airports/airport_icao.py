d6fa88a7e17a7f430680471314d89d10f9b8f8e7
Reketino <bjornevensk8@gmail.com>
2026-02-19 00:07:01 +0100

    Feat: new airport added

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -171,4 +171,10 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "country": "United States of America",
         "iata": "MIA",
    },
+    
+      "ENML": {
+        "name": "Molde Airport",
+        "country": "Norway",
+        "iata": "MOL",
+   },
 }
