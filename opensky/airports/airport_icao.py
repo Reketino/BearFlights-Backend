0c94587a5186721ad3f2471e891ad9c9bd6a2a58
Reketino <bjornevensk8@gmail.com>
2026-02-23 00:19:51 +0100

    Style: utils on airline

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -149,6 +149,11 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
    },
 
     # United Kingdom
+    "EGGD": {
+        "name": "Bristol Airport",
+        "country": "United Kingdom",
+        "iata": "BRS",
+   },
     "EGKK": {
         "name": "London Gatwick Airport",
         "country": "United Kingdom",
@@ -168,12 +173,6 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "name": "RAF Brize Norton",
         "country": "United Kingdom",
         "iata": "BZZ",
-   },
-    
-    "EGGD": {
-        "name": "Bristol Airport",
-        "country": "United Kingdom",
-        "iata": "BRS",
    },
 
     # United States of America
