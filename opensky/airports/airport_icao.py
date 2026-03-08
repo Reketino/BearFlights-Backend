c26281f387418253cef6c9e43cf7388b084e6465
Reketino <bjornevensk8@gmail.com>
2026-03-09 00:01:23 +0100

    Feat: new airport

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -7,6 +7,12 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "country": "Bulgaria",
         "iata": "",
    },
+    
+        "EFTP": {
+        "name": "Tampere-Pirkkala Airport",
+        "country": "Finland",
+        "iata": "TMP",
+   },
 
     # Denmark
     "EKBI": {
