8aeeeee3ddd26835b168b593b0084b78c70e4e38
Reketino <bjornevensk8@gmail.com>
2026-03-10 00:04:51 +0100

    Style: utils sorting on: aircraft_names

--- opensky/aircraft/aircraft_names.py
+++ opensky/aircraft/aircraft_names.py
@@ -44,9 +44,9 @@ AIRCRAFT_TYPES_BY_MANUFACTURER: dict[str, dict[str, str]] = {
     },
 
     "Cessna": {
+        "C177": "Cessna 177 Cardinal",
         "C25A": "Cessna 525A CJ2",
         "C56X": "Cessna Citation Excel",
-        "C177": "Cessna 177 Cardinal",
     },
 
     "De Havilland Canada": {
