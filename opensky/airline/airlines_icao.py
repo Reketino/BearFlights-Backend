9e24790af819b10768f1b361bd1701baa461b252
Reketino <bjornevensk8@gmail.com>
2026-03-23 00:00:59 +0100

    Feat: new airline added

--- opensky/airline/airlines_icao.py
+++ opensky/airline/airlines_icao.py
@@ -29,6 +29,7 @@ AIRLINES_BY_REGION: dict[str, dict[str, str]] = {
         "WUK": "Wizz Air UK",
         "BTI": "airBaltic",
         "EZY": "easyJet",
+        "WZZ": "Wizz Air",
     },
 
      "Middle East": {
