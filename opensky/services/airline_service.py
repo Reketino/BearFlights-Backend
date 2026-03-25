32e48b77ab8ebf37cc84b1300e4645dbd31cd079
Reketino <bjornevensk8@gmail.com>
2026-03-26 00:05:57 +0100

    Feat: setup return if callsign is not found

--- opensky/services/airline_service.py
+++ opensky/services/airline_service.py
@@ -4,5 +4,7 @@ from opensky.airline.airline import airline_from_callsign
 
 class AirlineService:
     def get_airlines(self, callsign: str) -> Optional[Tuple[str, str]]:
+        if not callsign:
+            return None
         
     
