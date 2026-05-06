9bdceff3b9e98d0fc154a2b11422cba6fe8f23d0
Reketino <bjornevensk8@gmail.com>
2026-05-07 00:04:10 +0200

    Feat: Added CargoJet company plus Canada as country in airlines_icao

--- opensky/airline/airlines_icao.py
+++ opensky/airline/airlines_icao.py
@@ -50,6 +50,10 @@ AIRLINES_BY_REGION: dict[str, dict[str, str]] = {
      "The United States of America": {
         "UAL": "United Airlines",
     },
+     
+     "Canada": {
+         "CJT": "Cargojet Airways",
+     },
 
      "Military / State": {
         "SVF": "Swedish Armed Forces",
