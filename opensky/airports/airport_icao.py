85f4302b4a54ab5be80c54e59228d5591fc9ba3d
Reketino <bjornevensk8@gmail.com>
2026-05-08 00:11:55 +0200

    Feat: new airport added

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -7,6 +7,12 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "country": "Bulgaria",
         "iata": "",
    },
+    
+      "EDDF": {
+        "name": "Frankfurt Airport",
+        "country": "Germany",
+        "iata": "FRA",
+        },
 
     # Denmark
     "EKBI": {
