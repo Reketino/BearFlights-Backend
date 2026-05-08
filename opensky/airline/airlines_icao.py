a60b037d4fdfcedb8f28b3a6ea525b0cab6610ac
Reketino <bjornevensk8@gmail.com>
2026-05-09 00:04:08 +0200

    Feat: Added Delta Airlines into airline database

--- opensky/airline/airlines_icao.py
+++ opensky/airline/airlines_icao.py
@@ -49,6 +49,7 @@ AIRLINES_BY_REGION: dict[str, dict[str, str]] = {
 
      "The United States of America": {
         "UAL": "United Airlines",
+        "DAL": "Delta Air Lines, Inc",
     },
 
      "Canada": {
