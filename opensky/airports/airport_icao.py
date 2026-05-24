78d47cad651718323540c6b9c89886f1406375e0
Reketino <bjornevensk8@gmail.com>
2026-05-24 10:49:57 +0200

    style: utils sorting on airports

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -7,12 +7,6 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "country": "Bulgaria",
         "iata": "",
    },
-        "ENBO": {
-        "name": "Bodø Airport",
-        "country": "Norway",
-        "iata": "BOO",
-   },
-
 
     # Canada
     "CYMX": {
@@ -155,6 +149,11 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "name": "Bergen Flesland Airport",
         "country": "Norway",
         "iata": "BGO",
+   },
+    "ENBO": {
+        "name": "Bodø Airport",
+        "country": "Norway",
+        "iata": "BOO",
    },
     "ENBL": {
         "name": "Førde Airport",
