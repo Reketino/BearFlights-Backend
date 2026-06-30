2f19fdfbd185419a1f0d118591ff2d3331a7ef1c
Reketino <bjornevensk8@gmail.com>
2026-07-01 00:00:16 +0200

    feat: Added new airline

--- opensky/airline/airlines_icao.py
+++ opensky/airline/airlines_icao.py
@@ -10,6 +10,7 @@ AIRLINES_BY_REGION: dict[str, dict[str, str]] = {
         "SAS": "Scandinavian Airlines",
         "VKG": "Sunclass Airlines",
         "WIF": "Widerøe",
+        "ABF": "Scanwings",
     },
 
      "Europe": {
