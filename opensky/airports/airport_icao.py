c154f80411135b92a6db5ac430ae4320ffd6213c
Reketino <bjornevensk8@gmail.com>
2026-07-05 00:00:04 +0200

    feat: new airport added

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -7,6 +7,11 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "country": "Bulgaria",
         "iata": "",
    },
+    "LGRP": {
+        "name": "Rhodes International Airport",
+        "country": "Greece",
+        "iata": "RHO",
+   },
 
     # Canada
     "CYMX": {
