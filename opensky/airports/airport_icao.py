c6358fab869cc2911605696f765169cea49b1ac3
Reketino <bjornevensk8@gmail.com>
2026-07-06 00:00:54 +0200

    feat: new airport added

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -7,6 +7,12 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "country": "Bulgaria",
         "iata": "",
    },
+    
+        "OMDB": {
+        "name": "Dubai International Airport",
+        "country": "United Arab Emirates",
+        "iata": "DXB",
+   },
 
     # Canada
     "CYMX": {
