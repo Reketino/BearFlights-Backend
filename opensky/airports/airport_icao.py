7989873c747a80466aba33fc1fddf4025a3d8f84
Reketino <bjornevensk8@gmail.com>
2026-07-07 00:01:07 +0200

    feat: new airport

--- opensky/airports/airport_icao.py
+++ opensky/airports/airport_icao.py
@@ -7,6 +7,12 @@ AIRPORTS_BY_ICAO: dict[str, dict[str, str]] = {
         "country": "Bulgaria",
         "iata": "",
    },
+    
+        "EGPH": {
+        "name": "Edinburgh Airport",
+        "country": "United Kingdom",
+        "iata": "EDI",
+   },
 
     # Canada
     "CYMX": {
