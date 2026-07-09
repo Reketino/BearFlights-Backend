10c4e57023ed642784887c9156f2821900d5dfca
Reketino <bjornevensk8@gmail.com>
2026-07-10 00:00:28 +0200

    feat: new airrline_icao added

--- opensky/airline/airlines_icao.py
+++ opensky/airline/airlines_icao.py
@@ -55,6 +55,7 @@ AIRLINES_BY_REGION: dict[str, dict[str, str]] = {
      "The United States of America": {
         "DAL": "Delta Air Lines, Inc",
         "UAL": "United Airlines",
+        "FJO": "FlexJet",
     },
 
      "Canada": {
